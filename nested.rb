fa94af0b114b6d3725302c09bdf9875d3a63df13
osagie obaretin <osagie.codes@gmail.com>
2020-06-07 21:31:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,3 +119,25 @@ end
 
 changing_alan
 
+def adding_to_dennis
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
+
+end
+
+adding_to_dennis
