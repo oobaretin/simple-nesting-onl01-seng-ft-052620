ee8ee5ee78c2ae0ed6f62775ba02976955116835
osagie obaretin <osagie.codes@gmail.com>
2020-06-07 21:28:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,3 +58,38 @@ def dennis_ritchies_language
 
 	programmer_hash[:dennis_ritchie][:languages][0]
 end
+
+def adding_matz
+
+
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
+    
+  programmer_hash
+    
+    
+# Here is the learn.co way of doing it.    
+# programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+# programmer_hash
+end
+
+adding_matz
